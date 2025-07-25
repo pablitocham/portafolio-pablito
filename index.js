73fbdd890d2d3d1635ea9b87b18e5e794e3c38b6
Juan Pablo Chamorro <jpablocham@gmail.com>
2025-07-24 23:03:40 -0300

    optimizacion

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
-function mostrarOcultar() {
-    let nav = document.getElementById("nav");
-    nav.classList.toggle("responsive");
+const nav = document.getElementById('nav');
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.querySelectorAll('nav, ul, li, a');
 
+
+function mostrarOcultar() {
+    nav.classList.toggle("mostrar");
 }
 
 function seleccionar() {
-    document.getElementById("nav").classList = ""
-    menuVisible = false
+    nav.classList.remove('mostrar')
 
 }
+
+function enlace(event) {
+    navLinks.forEach(link => link.classList.remove('active'));
+
+    if (event && event.target) {
+        event.target.classList.add('active')
+    }
+}
+
+menuToggle.addEventListener('click', mostrarOcultar)
+navLinks.forEach(link => {
+    link.addEventListener('click', function (event) {
+        seleccionar()
+        enlace(event)
+    })
+})
+
+window.addEventListener('resize', function () {
+    if (window.innerWidth >= 768) {
+        document.getElementById('nav').classList.remove('mostrar')
+    }
+})
+
+AOS.init({
+    duration: 1000,
+    once: true
+});
 
